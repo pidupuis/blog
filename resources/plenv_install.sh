e09bb3091b5ef7a56b1a1e2f6a0f3054290c7872
pidupuis <pierredupuis@openmailbox.org>
2017-01-04 08:22:09 +0100

    refactor(venv): update scripts

--- resources/plenv_install.sh
+++ resources/plenv_install.sh
@@ -1,20 +1,26 @@
+# Install env bin
 git clone https://github.com/tokuhirom/plenv.git ~/.plenv
 git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build
 
+# Add bin to your path
 echo 'export PATH="$HOME/.plenv/bin:$PATH"' >> ~/.bash_profile
 echo 'eval "$(plenv init -)"' >> ~/.bash_profile
-
 source ~/.bash_profile
 
+# Install a specific version
 plenv install 5.25.8
 plenv rehash
 
+# Set up env for a specific folder
 mkdir perl_project
 cd perl_project
 plenv local 5.25.8
 
+# Install package manager
 plenv install-cpanm
 
-echo "requires 'JSON';" >> cpanfile
+# Add a package to the listing file
 echo "requires 'DateTime';" >> cpanfile
+
+# Install packages locally from the listing file
 cpanm --installdeps .
--- resources/pyenv_install.sh
+++ resources/pyenv_install.sh
@@ -1,22 +1,28 @@
+# Install env bin
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
 
-
+# Add bin to your path
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bash_profile
 echo 'export PATH="$HOME/.pyenv/shims:$PATH"' >> ~/.bash_profile
 echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
-
 source ~/.bash_profile
 
+# Install a specific version
 pyenv install 3.5.1
 
+# Set up env for a specific folder
 mkdir python_project
 cd python_project
 pyenv virtualenv 3.5.1 python_project
 pyenv local python_project
 
+# Install package manager
 pip install --upgrade pip
 
+# Add a package to the listing file
 echo "python-dateutil" >> requirements.txt
+
+# Install packages locally from the listing file
 pip install -r requirements.txt
